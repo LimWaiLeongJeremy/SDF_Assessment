e00b0f5a051088cea7070b08cffe0cb560c58544
LimWaiLeongJeremy <jereremy19995@hotmail.sg>
2022-07-01 09:36:01 +0800

    first commit

---
+++ src/main/java/assessment/task1/App.java
@@ -0,0 +1,13 @@
+package assessment.task1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
