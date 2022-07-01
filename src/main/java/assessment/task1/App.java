2f675b07b9833c265c47f404f530ece88c6130e6
LimWaiLeongJeremy <jereremy19995@hotmail.sg>
2022-07-01 17:04:14 +0800

    first commit

--- src/main/java/assessment/task1/App.java
+++ src/main/java/assessment/task1/App.java
@@ -1,60 +1,69 @@
 package assessment.task1;
 
 import java.io.BufferedReader;
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
 
 
 public class App {
     
-    public static void main(String[] args) throws Exception {
-       
-        // Declare CSV & txt file path as a String
-        String csvFilePath = "/Users/jeremy/Desktop/asseesment/task.1/thankyou.csv";
-        String templateFilePath = "/Users/jeremy/Desktop/asseesment/task.1/thankyou.txt";
+    public static void main(String[] args) {
 
-        // reading csv files
-        BufferedReader csvReader = new BufferedReader(new FileReader(csvFilePath));
-        String csvLine = "";
+       try{
+            // Declare CSV & txt file path as a String
+            String csvFilePath = args[0];
+            String templateFilePath = args[1];
 
-        String[] tempArr;
-        List<String> values;
-                
-        while ((csvLine = csvReader.readLine()) != null) {
-            tempArr = csvLine.split(",");
-            values = new ArrayList<>(Arrays.asList(tempArr));
-        
-        
-        // System.out.println(values.get(1));
-        }
+                    
+            // readind txt files
+            File ogFile = new File(templateFilePath);
+            BufferedReader txtReader = new BufferedReader(new FileReader(ogFile));
+            String txtLine = null;
+            String oldContent1 = "__index1__";
+            String oldContent2 = "__index2__";
+            String oldContent3 = "__index3__";
+            String oldContent4 = "__index4__";
+
+            // reading csv files
+            BufferedReader csvReader = new BufferedReader(new FileReader(csvFilePath));
+            String csvLine = "";
+
+            String[] tempArr;
+            List<String> content;
+                    
+            while ((csvLine = csvReader.readLine()) != null) {
+                tempArr = csvLine.split(",");
+                content = new ArrayList<>(Arrays.asList(tempArr));   
+            
+                System.out.println();
+                while ((txtLine = txtReader.readLine()) != null) {       
+                    
+                    for (int i = 1; i < content.size(); i++)
+                    txtLine.replace(oldContent1, content.get(1));
+                    txtLine.replace(oldContent2, content.get(2));
+                    txtLine.replace(oldContent3, content.get(3));
+                    txtLine.replace(oldContent4, content.get(4));
+
+                    System.out.println(txtLine);
+                    txtLine = txtReader.readLine();
+
+                }    
+            }
         csvReader.close();
-
-        // readind txt files
-        File ogFile = new File(templateFilePath);
-        String oldContent = "";
-        BufferedReader txtReader = new BufferedReader(new FileReader(ogFile));
-        String line = txtReader.readLine();
-
-        while (line != null)
-        {
-                oldContent = oldContent + line + System.lineSeparator();
-                line = txtReader.readLine();
-        }
-        System.out.println(line);
-
         txtReader.close();
 
-
-
-    
+        } catch (FileNotFoundException e) {
+            System.err.println(e);
+        } catch (EOFException e) {
+            System.err.println(e);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
     }
 }  
        
